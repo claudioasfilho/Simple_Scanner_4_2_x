b8d3c22d83757e5f10ceec2ab89bf3eff90811ff
Claudio Filho <claudioasfilho@gmail.com>
2022-11-03 13:28:07 -0700

    first commit

---
+++ app.c
@@ -0,0 +1,203 @@
+/***************************************************************************//**
+ * @file
+ * @brief Empty NCP-host Example Project.
+ *
+ * Reference implementation of an NCP (Network Co-Processor) host, which is
+ * typically run on a central MCU without radio. It can connect to an NCP via
+ * VCOM to access the Bluetooth stack of the NCP and to control it using BGAPI.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ ******************************************************************************/
+#include <stdlib.h>
+#include <unistd.h>
+#include "app.h"
+#include "gatt_db.h"
+#include "ncp_host.h"
+#include "app_log.h"
+#include "app_log_cli.h"
+#include "app_assert.h"
+#include "sl_bt_api.h"
+
+// Optstring argument for getopt.
+#define OPTSTRING      NCP_HOST_OPTSTRING APP_LOG_OPTSTRING "h"
+
+// Usage info.
+#define USAGE          APP_LOG_NL "%s " NCP_HOST_USAGE APP_LOG_USAGE " [-h]" APP_LOG_NL
+
+// Options info.
+#define OPTIONS    \
+  "\nOPTIONS\n"    \
+  NCP_HOST_OPTIONS \
+  APP_LOG_OPTIONS  \
+  "    -h  Print this help message.\n"
+
+
+
+
+
+volatile int BleADVCounter = 0;
+volatile int counter = 0;
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+void app_init(int argc, char *argv[])
+{
+  sl_status_t sc;
+  int opt;
+
+  // Process command line options.
+  while ((opt = getopt(argc, argv, OPTSTRING)) != -1) {
+    switch (opt) {
+      // Print help.
+      case 'h':
+        app_log(USAGE, argv[0]);
+        app_log(OPTIONS);
+        exit(EXIT_SUCCESS);
+
+      // Process options for other modules.
+      default:
+        sc = ncp_host_set_option((char)opt, optarg);
+        if (sc == SL_STATUS_NOT_FOUND) {
+          sc = app_log_set_option((char)opt, optarg);
+        }
+        if (sc != SL_STATUS_OK) {
+          app_log(USAGE, argv[0]);
+          exit(EXIT_FAILURE);
+        }
+        break;
+    }
+  }
+
+ 
+
+  // Initialize NCP connection.
+  sc = ncp_host_init();
+  if (sc == SL_STATUS_INVALID_PARAMETER) {
+    app_log(USAGE, argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  app_assert_status(sc);
+  app_log_info("NCP host initialised." APP_LOG_NL);
+  app_log_info("Resetting NCP target..." APP_LOG_NL);
+  // Reset NCP to ensure it gets into a defined state.
+  // Once the chip successfully boots, boot event should be received.
+  sl_bt_system_reset(sl_bt_system_boot_mode_normal);
+
+  app_log_info("Press Crtl+C to quit" APP_LOG_NL APP_LOG_NL);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Application Deinit.
+ *****************************************************************************/
+void app_deinit(void)
+{
+  ncp_host_deinit();
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application deinit code here!                       //
+  // This is called once during termination.                                 //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+
+       sc = sl_bt_scanner_start(sl_bt_scanner_scan_phy_1m,
+                               sl_bt_scanner_discover_generic);
+      app_assert_status(sc);
+      app_log("Scanning started\r\n");
+
+
+      break;
+
+    case sl_bt_evt_scanner_legacy_advertisement_report_id:
+
+        printf("BLE device found\r\n");
+        printf (" rssi:%d  \r\n", evt->data.evt_scanner_legacy_advertisement_report.rssi );
+        printf (" address: %x6s \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address.addr);
+        printf (" address_type: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address_type);
+        printf (" bonding: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.bonding);
+        printf (" data:%xs \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.data.data);
+        printf("Advertisement Count: %d\r\n", BleADVCounter++ );
+      break;
+
+    // -------------------------------
+    // This event indicates that a new connection was opened.
+    case sl_bt_evt_connection_opened_id:
+      app_log_info("Connection opened." APP_LOG_NL);
+      break;
+
+    // -------------------------------
+    // This event indicates that a connection was closed.
+    case sl_bt_evt_connection_closed_id:
+      app_log_info("Connection closed." APP_LOG_NL);
+
+      break;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Add additional event handlers here as your application requires!      //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# SoC - Simple Bluetooth Scanner
+
+The Bluetooth example is a Simple Bluetooth Scanner that prints out all the advertisements it sees. 
+
+
+**Other details:**
+
+Hardware: EFR31MG21 - **BRD4180A**
+https://www.silabs.com/documents/public/schematic-files/BRD4180A-hw-design-pkg.zip 
+**Gecko SDK Suite Version: 4.1.2**: Amazon 202012.00, Bluetooth 4.2.0, Bluetooth Mesh 3.0.2, EmberZNet 7.1.2.0, Flex 3.4.2.0, HomeKit 1.2.2.0, MCU 6.3.1.0, Matter Demo, Micrium OS Kernel 5.13.10, OpenThread 2.1.2.0 (GitHub-2ce3d3bf0), Platform 4.1.0.0, USB 1.0.0.0, Wi-SUN 1.3.2.0, Z-Wave SDK 7.18.2.0
+
+Configured with: --prefix=/Library/Developer/CommandLineTools/usr --with-gxx-include-dir=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/c++/4.2.1
+Apple clang version 12.0.0 (clang-1200.0.32.29)
+Target: x86_64-apple-darwin19.6.0
+
+
+
+## Resources
+
+[Bluetooth Documentation](https://docs.silabs.com/bluetooth/latest/)
+
+[UG103.14: Bluetooth LE Fundamentals](https://www.silabs.com/documents/public/user-guides/ug103-14-fundamentals-ble.pdf)
+
+[QSG169: Bluetooth SDK v3.x Quick Start Guide](https://www.silabs.com/documents/public/quick-start-guides/qsg169-bluetooth-sdk-v3x-quick-start-guide.pdf)
+
+[UG434: Silicon Labs Bluetooth ® C Application Developer's Guide for SDK v3.x](https://www.silabs.com/documents/public/user-guides/ug434-bluetooth-c-soc-dev-guide-sdk-v3x.pdf)
+
+[Bluetooth Training](https://www.silabs.com/support/training/bluetooth)
+
