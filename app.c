99b7f14ba5b42918cb94be5a19ab84b336b3b4c3
Claudio Filho <claudioasfilho@gmail.com>
2022-11-07 14:48:51 -0800

    Reverting wrong commit

--- app.c
+++ app.c
@@ -29,7 +29,7 @@
  * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
- *
+ **
  ******************************************************************************/
 #include <stdlib.h>
 #include <unistd.h>
@@ -58,16 +58,8 @@
 
 
 
-static uint8_t find_service_in_advertisement(uint8_t *data, uint8_t len);
-// Health Thermometer service UUID defined by Bluetooth SIG
-static const uint8_t thermo_service[2] = { 0x09, 0x18 };
-// Temperature Measurement characteristic UUID defined by Bluetooth SIG
-static const uint8_t thermo_char[2] = { 0x1c, 0x2a };
-
-
 volatile int BleADVCounter = 0;
 volatile int counter = 0;
-uint8_t 	connection_handle_;
 
 /**************************************************************************//**
  * Application Init.
@@ -157,10 +149,9 @@ void app_deinit(void)
 void sl_bt_on_event(sl_bt_msg_t *evt)
 {
   sl_status_t sc;
-  uint8_t	connection_handle;
-  //bd_addr address;
-  //uint8_t address_type;
-  //uint8_t system_id[8];
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
 
   switch (SL_BT_MSG_ID(evt->header)) {
     // -------------------------------
@@ -178,86 +169,20 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
 
     case sl_bt_evt_scanner_legacy_advertisement_report_id:
 
-
-
-        if (evt->data.evt_scanner_legacy_advertisement_report.event_flags
-          == (SL_BT_SCANNER_EVENT_FLAG_CONNECTABLE | SL_BT_SCANNER_EVENT_FLAG_SCANNABLE)) {
-
-        
-        // If a thermometer advertisement is found...
-        if (find_service_in_advertisement(&(evt->data.evt_scanner_legacy_advertisement_report.data.data[0]),
-                                          evt->data.evt_scanner_legacy_advertisement_report.data.len) != 0) {
-
-          app_log("If a thermometer advertisement is found...\n\r");
-
-          printf("BLE device found\r\n");
-          printf (" rssi:%d  \r\n", evt->data.evt_scanner_legacy_advertisement_report.rssi );
-          printf (" address: %x6s \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address.addr);
-          printf (" address_type: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address_type);
-          printf (" bonding: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.bonding);
-          printf (" data:%xs \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.data.data);
-          printf("Advertisement Count: %d\r\n", BleADVCounter++ );
-
-          // then stop scanning for a while
-          sc = sl_bt_scanner_stop();
-          app_assert_status(sc);
-          // and connect to that device
-          app_log("Opening connection\n\r");
-
-
-        sc = sl_bt_connection_open (evt->data.evt_scanner_legacy_advertisement_report.address, 
-                                    evt->data.evt_scanner_legacy_advertisement_report.address_type, 
-                                    sl_bt_gap_phy_1m, &connection_handle_);
-         
-          app_assert_status(sc);
-
-          app_log("Opening process\n\r");
-        }
-      }
+        printf("BLE device found\r\n");
+        printf (" rssi:%d  \r\n", evt->data.evt_scanner_legacy_advertisement_report.rssi );
+        printf (" address: %x6s \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address.addr);
+        printf (" address_type: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.address_type);
+        printf (" bonding: %d  \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.bonding);
+        printf (" data:%xs \r\n" ,  evt->data.evt_scanner_legacy_advertisement_report.data.data);
+        printf("Advertisement Count: %d\r\n", BleADVCounter++ );
       break;
 
     // -------------------------------
     // This event indicates that a new connection was opened.
     case sl_bt_evt_connection_opened_id:
-      app_log("Connection opened\n\r");
-
-      connection_handle = evt->data.evt_connection_opened.connection;
-
-     
+      app_log_info("Connection opened." APP_LOG_NL);
       break;
-
-    case sl_bt_evt_gatt_service_id:
-      //app_log("Services Discovered\n\r");
-      app_log("Service Handle : %d Service UUID: %s \n\r",evt->data.evt_gatt_service.service, evt->data.evt_gatt_service.uuid);
-
-      break;
-
-    case sl_bt_evt_connection_parameters_id:
-     app_log("Connection Parameters exchange\n\r");
-
-     sl_bt_system_set_lazy_soft_timer(3*32768, 0, 0xaa,1);
-      
-    if (evt->data.evt_connection_parameters.txsize > 27)
-    {
-      app_log("Connection Parameters Set\n\r");
-    }
-    break;
-    
-    case sl_bt_evt_system_soft_timer_id:
-
-    sc = sl_bt_gatt_discover_primary_services(connection_handle); //sl_bt_gatt_discover_primary_services(evt->data.evt_connection_parameters.connection);
-       app_log(" Softimer SC=  %x \n\r", sc);
-    app_assert_status(sc);
-    
-    break;
-
-    case sl_bt_evt_gatt_procedure_completed_id:
-
-    sc = evt->data.evt_gatt_procedure_completed.result;
-    app_log(" sl_bt_evt_gatt_procedure_completed_id %x \n\r", sc);
-    app_assert_status(sc);
-
-    break;
 
     // -------------------------------
     // This event indicates that a connection was closed.
@@ -275,28 +200,4 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
     default:
       break;
   }
-}
-
-
-// Parse advertisements looking for advertised Health Thermometer service
-static uint8_t find_service_in_advertisement(uint8_t *data, uint8_t len)
-{
-  uint8_t ad_field_length;
-  uint8_t ad_field_type;
-  uint8_t i = 0;
-  // Parse advertisement packet
-  while (i < len) {
-    ad_field_length = data[i];
-    ad_field_type = data[i + 1];
-    // Partial ($02) or complete ($03) list of 16-bit UUIDs
-    if (ad_field_type == 0x02 || ad_field_type == 0x03) {
-      // compare UUID to Health Thermometer service UUID
-      if (memcmp(&data[i + 2], thermo_service, 2) == 0) {
-        return 1;
-      }
-    }
-    // advance to the next AD struct
-    i = i + ad_field_length + 1;
-  }
-  return 0;
 }
